1efbd3054276d0769ad9c48179ba1c233897ced8
Lyle <lyle.andrew19@gmail.com>
2024-02-08 16:26:16 -0500

    Testing

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -17,6 +17,7 @@ import ManufacturersList from './ManufacturerList';
 import AutomobileList from './AutomobileList';
 import AutomobileForm from './AutomobileForm';
 import ServiceHistoryList from './ServiceHistory';
+import CreateVehicleForm from './CreateVehicleForm';
 
 function App() {
   return (
@@ -25,32 +26,23 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-
           <Route path="salesperson/" element={<SalespersonList />} />
           <Route path="salesperson/create/" element={<CreateSalespersonForm />} />
-
           <Route path="customer/" element={<CustomerList />} />
           <Route path="customer/create/" element={<CreateCustomerForm />} />
-
           <Route path="sales/" element={<SalesList />} />
           <Route path="sales/create/" element={<CreateSaleForm />} />
           <Route path="sales/history/" element={<SalespersonHistoryList />} />
-
+          <Route path="vehicles/create/" element={<CreateVehicleForm />} />
           <Route path="technicians/" element={<TechnicianList />} />
           <Route path="technicians/create" element={<TechnicianForm />} />
-
           <Route path="appointments/" element={<AppointmentList />} />
           <Route path="appointments/create" element={<AppointmentForm />} />
-
-
+          <Route path="appointments/history" element={<ServiceHistoryList />} />
           <Route path="manufacturers" element={<ManufacturersList />} />
           <Route path="manufacturers/create" element={<ManufacturerForm />} />
           <Route path="automobiles" element={<AutomobileList />} />
           <Route path="automobiles/create" element={<AutomobileForm />} />
-          <Route path="appointments/history" element={<ServiceHistoryList />} />
-
-
-
         </Routes>
       </div>
     </BrowserRouter>
